5635483fc1eba80f48588102fb757f70e4a074cc
Fred Kafwembe <fredjkafwembe@gmail.com>
2019-05-13 13:25:53 +0200

    Updated database sql file

--- complexity_graphs.sql
+++ complexity_graphs.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 30, 2019 at 02:28 PM
--- Server version: 10.1.34-MariaDB
--- PHP Version: 7.2.8
+-- Generation Time: May 13, 2019 at 01:19 PM
+-- Server version: 10.1.39-MariaDB
+-- PHP Version: 7.1.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,19 +19,29 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `cs curriculum complexity graphs`
+-- Database: `complexity_graphs`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `courses`
+-- Table structure for table `course`
 --
 
-CREATE TABLE `courses` (
-  `course _description` text NOT NULL,
-  `course_code` varchar(7) NOT NULL
+CREATE TABLE `course` (
+  `course_description` text,
+  `course_code` varchar(7) NOT NULL,
+  `year` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`course_description`, `course_code`, `year`) VALUES
+('Basics of programming with java.', 'CSC2000', 2),
+('CSC2202', 'CSC2202', 2),
+('Software engineering.', 'CSC3600', 3);
 
 -- --------------------------------------------------------
 
@@ -45,13 +55,20 @@ CREATE TABLE `prerequisites` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `prerequisites`
+--
+
+INSERT INTO `prerequisites` (`course_code`, `prerequisite_code`) VALUES
+('CSC3600', 'CSC2000');
+
+--
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `courses`
+-- Indexes for table `course`
 --
-ALTER TABLE `courses`
+ALTER TABLE `course`
   ADD PRIMARY KEY (`course_code`);
 
 --
@@ -66,16 +83,11 @@ ALTER TABLE `prerequisites`
 --
 
 --
--- Constraints for table `courses`
---
-ALTER TABLE `courses`
-  ADD CONSTRAINT `courses_ibfk_1` FOREIGN KEY (`course_code`) REFERENCES `prerequisites` (`prerequisite_code`);
-
---
 -- Constraints for table `prerequisites`
 --
 ALTER TABLE `prerequisites`
-  ADD CONSTRAINT `prerequisites_ibfk_1` FOREIGN KEY (`course_code`) REFERENCES `courses` (`course_code`);
+  ADD CONSTRAINT `prerequisites_ibfk_1` FOREIGN KEY (`course_code`) REFERENCES `course` (`course_code`),
+  ADD CONSTRAINT `prerequisites_ibfk_2` FOREIGN KEY (`prerequisite_code`) REFERENCES `course` (`course_code`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
